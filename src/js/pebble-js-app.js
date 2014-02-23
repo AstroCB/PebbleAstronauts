f5d9227808c5d00c9cd86f74e63e3db48a9b4a24
AstronomyiPhone <cambernhardt@me.com>
2014-02-23 15:44:06 -0800

    Added New Version (See bottom of JS file)
    I'm working on a new version that handles craft checking in case astronauts are in different places. There appears to be an issue with PebbleJS that prevents it from showing multiple notifications, but that isn't a problem. The problems lies in the fact that it will not even allow you to have multiple Pebble.showSimpleNotificationOnPebble() statements, even if all but one are unreachable. Any help is appreciated, but the new version should be up and running by next week.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -34,3 +34,47 @@ var req = new XMLHttpRequest();
     }
   }
   req.send(null);
+
+
+//New version - doesn't appear to be working at this time; I'm still working on it, but if anyone wants to give it a go, you're welcome to.
+
+/*Pebble.showSimpleNotification("Working?", "Yes");
+var req = new XMLHttpRequest();
+  req.open('GET', 'http://api.open-notify.org/astros.json', true);
+  req.onload = function(e) {
+    if (req.readyState == 4 && req.status == 200) {
+      if(req.status == 200) {
+        var response = JSON.parse(req.responseText);
+		var people = response.people;
+		var pplString = "";
+		var sameCraft = true;
+		var prevCraft = people[0].craft;
+		var craftString = "";
+		  for(var i = 0; i < people.length; i++){
+			  pplString += people[i].name;
+			  if(!(i === people.length - 1)){
+			  	pplString += ", ";
+			  }
+			  if(sameCraft && people[i].craft === prevCraft){
+				  prevCraft = people[i].craft;
+			  }else{
+				  sameCraft = false;
+			  }
+		  }
+		  if(sameCraft){
+	          //Pebble.showSimpleNotificationOnPebble("How many people are in space?",  response.number + "- " + pplString + ". They all currently reside on the " + people[0].craft + ".");
+		  }else{
+			  for(int i = 0; i < people.length; i++){
+				  if(i !== people.length - 1){
+					  craftString += people[i] + ": " + people[i].craft + "\n";
+				  }else{
+					  craftString += people[i] + ": " + people[i].craft;
+				  }
+			  }
+			  //Pebble.showSimpleNotificationOnPebble("How many people are in space?", response.number + "- " + pplString + "." + "\nLocations:\n" + craftString);
+		  }
+      } else { console.log("Error"); }
+    }
+  }
+  req.send(null);
+  */
