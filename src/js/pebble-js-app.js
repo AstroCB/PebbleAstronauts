08ac4eac9918656a6e1259a48d8d599654721b67
Cameron Bernhardt <cambernhardt@me.com>
2014-03-04 05:34:42 -0800

    Now craft-responsive
    The ISS is no longer hard-coded as the location; the app will now update based on the astronauts' locations in space.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -7,47 +7,6 @@ var req = new XMLHttpRequest();
 		var people = response.people;
 		var pplString = "";
 		var sameCraft = true;
-		var prevCraft = "";
-		  for(var i = 0; i < people.length; i++){
-			  pplString += people[i].name;
-			  if(!(i === people.length - 1)){
-			  	pplString += ", ";
-			  }
-			  /* while(sameCraft){
-				  if(prevCraft === people[i].craft){
-					  prevCraft = people[i].craft;
-					  break;
-				  }else{
-					  sameCraft = false;
-				  }
-			  }
-		  */}
-		  //if(sameCraft){
-	          Pebble.showSimpleNotificationOnPebble("How many people are in space?", response.number + "- " + pplString + ". They all currently reside on the " + people[0].craft + ".");
-		  //}else{
-			  //Pebble.showSimpleNotificationOnPebble("How many people are in space?", response.number + "- " + pplString + ".");
-			  //for(int i = 0; i < people.length; i++){
-				//  Pebble.showSimpleNotificationOnPebble(people[i] + ": " + people[i].craft);
-			  //}
-		  //}
-      } else { console.log("Error"); }
-    }
-  }
-  req.send(null);
-
-
-//New version - doesn't appear to be working at this time; I'm still working on it, but if anyone wants to give it a go, you're welcome to.
-
-/*Pebble.showSimpleNotification("Working?", "Yes");
-var req = new XMLHttpRequest();
-  req.open('GET', 'http://api.open-notify.org/astros.json', true);
-  req.onload = function(e) {
-    if (req.readyState == 4 && req.status == 200) {
-      if(req.status == 200) {
-        var response = JSON.parse(req.responseText);
-		var people = response.people;
-		var pplString = "";
-		var sameCraft = true;
 		var prevCraft = people[0].craft;
 		var craftString = "";
 		  for(var i = 0; i < people.length; i++){
@@ -62,19 +21,18 @@ var req = new XMLHttpRequest();
 			  }
 		  }
 		  if(sameCraft){
-	          //Pebble.showSimpleNotificationOnPebble("How many people are in space?",  response.number + "- " + pplString + ". They all currently reside on the " + people[0].craft + ".");
+	          Pebble.showSimpleNotificationOnPebble("How many people are in space?",  response.number + "- " + pplString + ". They all currently reside on the " + people[0].craft + ".");
 		  }else{
-			  for(int i = 0; i < people.length; i++){
+			  for(var i = 0; i < people.length; i++){
 				  if(i !== people.length - 1){
-					  craftString += people[i] + ": " + people[i].craft + "\n";
+					  craftString += people[i].name + ": " + people[i].craft + "\n";
 				  }else{
-					  craftString += people[i] + ": " + people[i].craft;
+					  craftString += people[i].name + ": " + people[i].craft;
 				  }
 			  }
-			  //Pebble.showSimpleNotificationOnPebble("How many people are in space?", response.number + "- " + pplString + "." + "\nLocations:\n" + craftString);
+			  Pebble.showSimpleNotificationOnPebble("How many people are in space?", response.number + "- " + pplString + ".\n\nLocations:\n" + craftString);
 		  }
       } else { console.log("Error"); }
     }
   }
   req.send(null);
-  */
